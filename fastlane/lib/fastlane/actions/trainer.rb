89bea6b2cfa4f131781cbf0aa3aa51f8edf74ec8
Alfredo Fernandes <afernandes@touchbistro.com>
2024-08-27 17:14:15 -0400

    Add trainer log

--- fastlane/lib/fastlane/actions/trainer.rb
+++ fastlane/lib/fastlane/actions/trainer.rb
@@ -9,7 +9,10 @@ module Fastlane
 
         fail_build = params[:fail_build]
         resulting_paths = Trainer::TestParser.auto_convert(params)
-        puts "resulting_paths: #{resulting_paths}"
+
+        UI.message("--> We are here")
+        UI.message("resulting_paths: #{resulting_paths}")
+
         resulting_paths.each do |path, test_results|
           UI.test_failure!("Unit tests failed") if fail_build && !test_results[:successful]
         end
